9b530f62d45e6b0974b06766c43d7c3d3e716634
Paul Highum <phigh07@gmail.com>
2018-04-28 16:49:49 -0600

    adds frame cards

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 document.querySelector("#player-num-form").addEventListener("submit", saveNumOfPlayers)
 
 function saveNumOfPlayers(e){
+  e.preventDefault()
+  document.querySelector("main").innerHTML = ""
   let numOfPlayers = e.target.childNodes[3].value
   for(var i = 0; i < numOfPlayers; i++){
     let playerCard = document.createElement("div")
@@ -8,13 +10,18 @@ function saveNumOfPlayers(e){
     let player = document.createElement("h1")
     player.innerText = `Player ${i + 1}`
     playerCard.appendChild(player)
+    createFrameCards(playerCard)
     document.querySelector("main").appendChild(playerCard)
   }
 }
 
-function createRoundCards(div){
+function createFrameCards(div){
   for(var j = 0; j < 10; j++){
-    let roundCard = document.createElement("div")
-    roundCard.setAttribute("class", j)  
+    let frameCard = document.createElement("div")
+    frameCard.setAttribute("class", `${j +1} frame-card`)
+    let frameLabel = document.createElement("h2")
+    frameLabel.innerText = `Frame ${j + 1}`
+    frameCard.appendChild(frameLabel)
+    div.appendChild(frameCard)
   }
 }
