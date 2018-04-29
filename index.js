f5d2f9d474f68ef3f20a60ba4bfce0450c6d05e2
Paul Highum <phigh07@gmail.com>
2018-04-29 13:48:01 -0600

    adds total score correctly, except last frame

--- index.js
+++ index.js
@@ -25,7 +25,17 @@ function createPlayerCards(num){
     playerCard.setAttribute("class", "player-card")
     let player = document.createElement("h1")
     player.innerText = `Player ${i + 1}`
+    let totalScoreDiv = document.createElement("div")
+    totalScoreDiv.setAttribute("class", "total-score-div")
+    let totalScore = document.createElement("p")
+    totalScore.setAttribute("class", "total-score")
+    let totalScoreLabel = document.createElement("h3")
+    totalScoreLabel.setAttribute("class", "total-score-label")
+    totalScoreLabel.innerText = "Total Score"
+    totalScoreDiv.appendChild(totalScoreLabel)
+    totalScoreDiv.appendChild(totalScore)
     playerCard.appendChild(player)
+    playerCard.appendChild(totalScoreDiv)
     createFrameCards(playerCard, i)
     document.querySelector("main").appendChild(playerCard)
   }
@@ -43,10 +53,7 @@ function createFrameCards(div, i){
     frameScore2.setAttribute("class", "frame-score-2")
     let frameScoreDiv = document.createElement("div")
     frameScoreDiv.setAttribute("class", "frame-score-div")
-    let totalScore = document.createElement("p")
-    totalScore.setAttribute("class", "total-score")
     frameCard.appendChild(frameLabel)
-    frameCard.appendChild(totalScore)
     frameScoreDiv.appendChild(frameScore1)
     frameScoreDiv.appendChild(frameScore2)
     frameCard.appendChild(frameScoreDiv)
@@ -58,32 +65,99 @@ function addScore(e){
   e.preventDefault()
   let score = Number(e.target.childNodes[3].value)
   let currentPlayerScoreArr = scoreArray[currentPlayer - 1]
-  currentPlayerScoreArr.push(score)
   addScoreToHTML(currentPlayerScoreArr, score)
-  console.log(scoreArray)
+  calcTotalScore(currentPlayerScoreArr)
 }
 
 function addScoreToHTML(currentPlayerScoreArr, score){
   if(score === 10){
     currentBowl = 1
-    document.querySelector(`.player${currentPlayer}frame${currentFrame}`).childNodes[2].childNodes[0].innerText = "X"
+    document.querySelector(`.player${currentPlayer}frame${currentFrame}`).childNodes[1].childNodes[0].innerText = "X"
+    currentPlayerScoreArr.push("X")
     setCurrentPlayer()
   } else if(currentBowl === 1){
-    document.querySelector(`.player${currentPlayer}frame${currentFrame}`).childNodes[2].childNodes[0].innerText = score
+    document.querySelector(`.player${currentPlayer}frame${currentFrame}`).childNodes[1].childNodes[0].innerText = score
     document.querySelector("#input-score").setAttribute("max", 10 - score)
+    currentPlayerScoreArr.push(score)
     currentBowl = 2
     document.querySelector(".score-label").innerText = `Player ${currentPlayer}, Frame ${currentFrame}, Bowl ${currentBowl} Score:`
-  } else if(currentBowl === 2 && currentPlayerScoreArr[currentPlayerScoreArr.length - 2] + score === 10){
-    document.querySelector(`.player${currentPlayer}frame${currentFrame}`).childNodes[2].childNodes[1].innerText = "/"
+  } else if(currentBowl === 2 && currentPlayerScoreArr[currentPlayerScoreArr.length - 1] + score === 10){
+    document.querySelector(`.player${currentPlayer}frame${currentFrame}`).childNodes[1].childNodes[1].innerText = "/"
+    currentPlayerScoreArr.push("/")
     currentBowl = 1
     document.querySelector("#input-score").setAttribute("max", 10)
     setCurrentPlayer()
   } else {
-    document.querySelector(`.player${currentPlayer}frame${currentFrame}`).childNodes[2].childNodes[1].innerText = score
+    document.querySelector(`.player${currentPlayer}frame${currentFrame}`).childNodes[1].childNodes[1].innerText = score
+    currentPlayerScoreArr.push(score)
     currentBowl = 1
     document.querySelector("#input-score").setAttribute("max", 10)
     setCurrentPlayer()
   }
+}
+
+function calcTotalScore(currentPlayerScoreArr){
+  let total = 0
+  for(var i = 0; i < currentPlayerScoreArr.length; i++){
+    if(currentPlayerScoreArr[i] === "X"){
+      if(currentPlayerScoreArr.length === i + 1){
+        total += 10
+      } else if(currentPlayerScoreArr.length === i + 2){
+        if(currentPlayerScoreArr[i + 1] === "X"){
+          total += 20
+        } else {
+          total += 10 + currentPlayerScoreArr[i + 1]
+        }
+      } else if(currentPlayerScoreArr.length >= i + 3){
+        if(currentPlayerScoreArr[i + 1] === "X" && currentPlayerScoreArr[i + 2] === "X"){
+          total += 30
+        } else if(currentPlayerScoreArr[i + 1] === "X" && currentPlayerScoreArr[i + 2] != "X"){
+          total += 20 + currentPlayerScoreArr[i + 1]
+        } else if(currentPlayerScoreArr[i + 1] != "X" && currentPlayerScoreArr[i + 2] === "/"){
+          total += 20
+        } else {
+          total += 10 + currentPlayerScoreArr[i + 1] + currentPlayerScoreArr[i + 2]
+        }
+      }
+    } else if(currentPlayerScoreArr[i] === "/"){
+      if(currentPlayerScoreArr.length === i + 1){
+        total += 10
+      } else if(currentPlayerScoreArr[i + 1] === "X"){
+        total += 20
+      } else {
+        total += 10 + currentPlayerScoreArr[i + 1]
+      }
+    } else if(currentPlayerScoreArr[i + 1] === "/"){
+      total += 0
+    } else {
+      total += currentPlayerScoreArr[i]
+    }
+    
+  }
+  console.log(total)
+}
+
+function calculateStrikes(currentPlayerScoreArr, i, total){
+  if(currentPlayerScoreArr[i] === "X" && currentPlayerScoreArr.length === i + 1){
+    total += 10
+  } else if(currentPlayerScoreArr[i] === "X" && currentPlayerScoreArr.length === i + 2){
+    if(currentPlayerScoreArr[i + 1] === "X"){
+      total += 20
+    } else {
+      total += 10 + currentPlayerScoreArr[i + 1]
+    }
+  } else if(currentPlayerScoreArr[i] === "X" && currentPlayerScoreArr.length >= i + 3){
+    if(currentPlayerScoreArr[i + 1] === "X" && currentPlayerScoreArr[i + 2] === "X"){
+      total += 30
+    } else if(currentPlayerScoreArr[i + 1] === "X" && currentPlayerScoreArr[i + 2] != "X"){
+      total += 20 + currentPlayerScoreArr[i + 1]
+    } else if(currentPlayerScoreArr[i + 1] != "X" && currentPlayerScoreArr[i + 2] === "/"){
+      total += 20
+    } else {
+      total += 10 + currentPlayerScoreArr[i + 1] + currentPlayerScoreArr[i + 2]
+    }
+  }
+  return total
 }
 
 function setCurrentPlayer(){
