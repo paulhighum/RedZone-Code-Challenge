f52389aa222629698e98f14edc1aca201294d8a1
Paul Highum <phigh07@gmail.com>
2018-04-29 11:40:30 -0600

    adds score for frame to page

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let totalFrames = 0
 let currentFrame = 1
 let currentPlayer = 1
 let currentBowl = 1
+let scoreArray =[]
 
 function saveNumOfPlayers(e){
   e.preventDefault()
@@ -18,50 +19,69 @@ function saveNumOfPlayers(e){
 
 function createPlayerCards(num){
   for(var i = 0; i < num; i++){
+    scoreArray.push([])
     let playerCard = document.createElement("div")
-    playerCard.setAttribute("id", i + 1)
+    playerCard.setAttribute("id", `player${i + 1}`)
     playerCard.setAttribute("class", "player-card")
     let player = document.createElement("h1")
     player.innerText = `Player ${i + 1}`
     playerCard.appendChild(player)
-    createFrameCards(playerCard)
+    createFrameCards(playerCard, i)
     document.querySelector("main").appendChild(playerCard)
   }
 }
 
-function createFrameCards(div){
+function createFrameCards(div, i){
   for(var j = 0; j < 10; j++){
     let frameCard = document.createElement("div")
-    frameCard.setAttribute("class", `${j +1} frame-card`)
+    frameCard.setAttribute("class", `player${i + 1}frame${j + 1} frame-card`)
     let frameLabel = document.createElement("h2")
     frameLabel.innerText = `Frame ${j + 1}`
     let frameScore1 = document.createElement("p")
     frameScore1.setAttribute("class", "frame-score-1")
     let frameScore2 = document.createElement("p")
     frameScore2.setAttribute("class", "frame-score-2")
+    let frameScoreDiv = document.createElement("div")
+    frameScoreDiv.setAttribute("class", "frame-score-div")
     let totalScore = document.createElement("p")
     totalScore.setAttribute("class", "total-score")
     frameCard.appendChild(frameLabel)
-    frameCard.appendChild(frameScore1)
-    frameCard.appendChild(frameScore2)
     frameCard.appendChild(totalScore)
+    frameScoreDiv.appendChild(frameScore1)
+    frameScoreDiv.appendChild(frameScore2)
+    frameCard.appendChild(frameScoreDiv)
     div.appendChild(frameCard)
   }
 }
 
 function addScore(e){
   e.preventDefault()
-  
-  setCurrentBowl()
+  let score = Number(e.target.childNodes[3].value)
+  let currentPlayerScoreArr = scoreArray[currentPlayer - 1]
+  currentPlayerScoreArr.push(score)
+  addScoreToHTML(currentPlayerScoreArr, score)
+  console.log(scoreArray)
 }
 
-function setCurrentBowl(){
-  document.querySelector(".score-label").innerText = ""
-  if(currentBowl === 1){
+function addScoreToHTML(currentPlayerScoreArr, score){
+  if(score === 10){
+    currentBowl = 1
+    document.querySelector(`.player${currentPlayer}frame${currentFrame}`).childNodes[2].childNodes[0].innerText = "X"
+    setCurrentPlayer()
+  } else if(currentBowl === 1){
+    document.querySelector(`.player${currentPlayer}frame${currentFrame}`).childNodes[2].childNodes[0].innerText = score
+    document.querySelector("#input-score").setAttribute("max", 10 - score)
     currentBowl = 2
     document.querySelector(".score-label").innerText = `Player ${currentPlayer}, Frame ${currentFrame}, Bowl ${currentBowl} Score:`
-  } else {
+  } else if(currentBowl === 2 && currentPlayerScoreArr[currentPlayerScoreArr.length - 2] + score === 10){
+    document.querySelector(`.player${currentPlayer}frame${currentFrame}`).childNodes[2].childNodes[1].innerText = "/"
     currentBowl = 1
+    document.querySelector("#input-score").setAttribute("max", 10)
+    setCurrentPlayer()
+  } else {
+    document.querySelector(`.player${currentPlayer}frame${currentFrame}`).childNodes[2].childNodes[1].innerText = score
+    currentBowl = 1
+    document.querySelector("#input-score").setAttribute("max", 10)
     setCurrentPlayer()
   }
 }
